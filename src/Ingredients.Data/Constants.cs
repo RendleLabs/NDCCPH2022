401a1e69ebd202140d90bcf6f6a6a5944d622c10
Mark Rendle <mark@rendlelabs.com>
2022-05-28 14:50:25 +0100

    Initial commit

---
+++ src/Ingredients.Data/Constants.cs
@@ -0,0 +1,6 @@
+namespace Ingredients.Data;
+
+public static class Constants
+{
+    public const string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=ndccph;AccountKey=BTAVWH/nv3uKvZnPOGbmtAJRJ6ejXFbsawJvra3bUuY34QTUY9BsneVfecvA1VeKSoXwNBN7gyzA+ASteVHmRQ==;EndpointSuffix=core.windows.net";
+}
---
+++ src/Orders.PubSub/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace Orders.PubSub;
+
+internal static class Constants
+{
+    public const string ConnectionString = "ndccph.redis.cache.windows.net:6380,password=8mi4uq1JDjr64sBDTL8NxXNK6dBTG3NoMAzCaBfakcw=,ssl=True,abortConnect=False";
+}
