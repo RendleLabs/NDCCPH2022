cd635b74499c2aaaad637c6c89a473d30b7675dd
Mark Rendle <mark@rendlelabs.com>
2022-05-30 16:00:00 +0200

    ShopConsole

--- src/ShopConsole/Program.cs
+++ src/ShopConsole/Program.cs
@@ -1,8 +1,18 @@
+using Orders.Protos;
 using ShopConsole;
+
+var binding = OperatingSystem.IsMacOS() ? "http" : "https";
+var defaultUri = OperatingSystem.IsMacOS() ? "http://localhost:5004" : "https://localhost:5005";
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
+        services.AddGrpcClient<OrderService.OrderServiceClient>((provider, options) =>
+        {
+            var configuration = provider.GetRequiredService<IConfiguration>();
+            var uri = configuration.GetServiceUri("orders", binding) ?? new Uri(defaultUri);
+            options.Address = uri;
+        });
         services.AddHostedService<Worker>();
     })
     .Build();
--- src/ShopConsole/Worker.cs
+++ src/ShopConsole/Worker.cs
@@ -1,20 +1,47 @@
+using Grpc.Core;
+using Orders.Protos;
+
 namespace ShopConsole;
 
 public class Worker : BackgroundService
 {
+    private readonly OrderService.OrderServiceClient _orders;
     private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker( 
+        OrderService.OrderServiceClient orders,
+        ILogger<Worker> logger)
     {
+        _orders = orders;
         _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            try
+            {
+                var call = _orders.Subscribe(new SubscribeRequest(), cancellationToken: stoppingToken);
+                var stream = call.ResponseStream;
+
+                await foreach (var notification in stream.ReadAllAsync(stoppingToken))
+                {
+                    _logger.LogInformation("Order: {CrustIds} with {ToppingIds} due by {DueBy}",
+                        notification.CrustId,
+                        string.Join(", ", notification.ToppingIds),
+                        notification.DueBy.ToDateTimeOffset().ToLocalTime().ToString("t"));
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                if (stoppingToken.IsCancellationRequested) break;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
         }
     }
 }
