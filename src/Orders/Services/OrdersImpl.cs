1a938c4a70c611c492946d4d395b90546866188b
Mark Rendle <mark@rendlelabs.com>
2022-05-30 15:34:37 +0200

    Orders Subscribe

--- src/Orders/Services/OrdersImpl.cs
+++ src/Orders/Services/OrdersImpl.cs
@@ -2,33 +2,89 @@
 using Grpc.Core;
 using Ingredients.Protos;
 using Orders.Protos;
+using Orders.PubSub;
 
 namespace Orders.Services;
 
 public class OrdersImpl : OrderService.OrderServiceBase
 {
     private readonly IngredientsService.IngredientsServiceClient _ingredients;
+    private readonly IOrderPublisher _orderPublisher;
+    private readonly IOrderMessages _orderMessages;
+    private readonly ILogger<OrdersImpl> _logger;
 
-    public OrdersImpl(IngredientsService.IngredientsServiceClient ingredients)
+    public OrdersImpl(IngredientsService.IngredientsServiceClient ingredients,
+        IOrderPublisher orderPublisher, IOrderMessages orderMessages, ILogger<OrdersImpl> logger)
     {
         _ingredients = ingredients;
+        _orderPublisher = orderPublisher;
+        _orderMessages = orderMessages;
+        _logger = logger;
     }
+    
     public override async Task<PlaceOrderResponse> PlaceOrder(PlaceOrderRequest request, ServerCallContext context)
     {
-        await _ingredients.DecrementToppingsAsync(new DecrementToppingsRequest
+        try
         {
-            ToppingIds = {request.ToppingIds}
-        });
-        await _ingredients.DecrementCrustsAsync(new DecrementCrustsRequest
-        {
-            CrustId = request.CrustId
-        });
+            await _ingredients.DecrementToppingsAsync(new DecrementToppingsRequest
+            {
+                ToppingIds = {request.ToppingIds}
+            });
+            await _ingredients.DecrementCrustsAsync(new DecrementCrustsRequest
+            {
+                CrustId = request.CrustId
+            });
 
-        var dueBy = DateTimeOffset.UtcNow.AddMinutes(45);
+            var dueBy = DateTimeOffset.UtcNow.AddMinutes(45);
 
-        return new PlaceOrderResponse
+            await _orderPublisher.PublishOrder(request.CrustId, request.ToppingIds, dueBy);
+
+            return new PlaceOrderResponse
+            {
+                DueBy = dueBy.ToTimestamp()
+            };
+        }
+        catch (Exception e)
         {
-            DueBy = dueBy.ToTimestamp()
-        };
+            _logger.LogError(e, e.Message);
+            throw new RpcException(new Status(StatusCode.Internal, e.Message, e));
+        }
+    }
+
+    public override async Task Subscribe(SubscribeRequest request,
+        IServerStreamWriter<OrderNotification> responseStream,
+        ServerCallContext context)
+    {
+        var cancellationToken = context.CancellationToken;
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var message = await _orderMessages.ReadAsync(cancellationToken);
+                var notification = new OrderNotification
+                {
+                    CrustId = message.CrustId,
+                    ToppingIds = {message.ToppingIds},
+                    DueBy = message.Time.ToTimestamp(),
+                };
+                try
+                {
+                    await responseStream.WriteAsync(notification, cancellationToken);
+                }
+                catch
+                {
+                    await _orderPublisher.PublishOrder(message.CrustId, message.ToppingIds, message.Time);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+            }
+        }
     }
 }
---
+++ src/ShopConsole/Program.cs
@@ -0,0 +1,10 @@
+using ShopConsole;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
