bca8d3868180bc15bfc5a7cc360043c6b3142128
Mark Rendle <mark@rendlelabs.com>
2022-05-30 14:21:53 +0200

    Decrement things

---
+++ src/Orders/Services/OrdersImpl.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using Orders.Protos;
+
+namespace Orders.Services;
+
+public class OrdersImpl : OrderService.OrderServiceBase
+{
+    public override Task<PlaceOrderResponse> PlaceOrder(PlaceOrderRequest request, ServerCallContext context)
+    {
+        return base.PlaceOrder(request, context);
+    }
+}
