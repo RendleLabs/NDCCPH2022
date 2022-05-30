db5dbaad4159a857724112b5f33c04357cf2faa0
Mark Rendle <mark@rendlelabs.com>
2022-05-30 10:02:22 +0200

    ingredients.proto

---
+++ src/Ingredients/Services/IngredientsImpl.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+using Ingredients.Protos;
+
+namespace Ingredients.Services;
+
+public class IngredientsImpl : IngredientsService.IngredientsServiceBase
+{
+    public override Task<GetToppingsResponse> GetToppings(GetToppingsRequest request, ServerCallContext context)
+    {
+        return base.GetToppings(request, context);
+    }
+}
